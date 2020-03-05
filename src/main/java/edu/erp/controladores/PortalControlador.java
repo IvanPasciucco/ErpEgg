41c846628a849cf993da7eafc2a4d6094eb8639d
ivan <pasciucco14@gmail.com>
2020-03-05 13:22:13 -0300

    sigo subiendo mierdas, tincho termina el controlador culiao

--- src/main/java/edu/erp/controladores/PortalControlador.java
+++ src/main/java/edu/erp/controladores/PortalControlador.java
@@ -1,7 +1,10 @@
 package edu.erp.controladores;
 
+import edu.erp.entidades.errores.ErrorServicio;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 
@@ -13,4 +16,29 @@ public class PortalControlador {
     public String index(){
         return "index.html";
     }
+     @GetMapping("/about")
+    public String about(){
+        return "about.html";
+    }
+    @GetMapping("/registro")
+   public String registro() {
+       return "registro.html";
+  }
+   @GetMapping("/login")
+   public String login() {
+       return "login.html";
+  }
+    @PostMapping("/registrar")
+   public String registrar(ModelMap modelo){
+       System.out.println();//           modelo.put("error", ex.getMessage());
+//           modelo.put("nombre", nombre);
+//           modelo.put("apellido", apellido);
+//           modelo.put("mail", mail);
+//           modelo.put("clave", clave);
+//           modelo.put("zona", idZona);
+//           modelo.put("grupo_sanguineo", grupo_sanguineo);
+//           modelo.put("requisitos", id_requisitos);
+       return "index.html";
+       }
+    
 }
