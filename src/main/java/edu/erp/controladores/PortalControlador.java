f091d0aa216abdd9cb821e1b4151e7654ea8bebf
ivan <pasciucco14@gmail.com>
2020-03-06 06:06:13 -0300

    toma tu contolador

--- src/main/java/edu/erp/controladores/PortalControlador.java
+++ src/main/java/edu/erp/controladores/PortalControlador.java
@@ -3,12 +3,15 @@ package edu.erp.controladores;
 import edu.erp.entidades.errores.ErrorServicio;
 import edu.erp.servicios.UsuarioServicio;
 import java.util.Date;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 
 @Controller
@@ -31,6 +34,18 @@ public class PortalControlador {
    public String registro() {
        return "registro.html";
   }
+    @PostMapping("/registrar")
+   public String registrar(ModelMap modelo, @RequestParam String nombre, @RequestParam  String email, @RequestParam  String tel, @RequestParam String clave){
+        try {
+            usuarioServicio.registrar(nombre, email, tel, clave);
+        } catch (ErrorServicio ex) {
+            return "registro.html";
+    //        Logger.getLogger(PortalControlador.class.getName()).log(Level.SEVERE, null, ex);
+        }
+   
+       return "index.html";
+      
+   }
    
    @GetMapping("/shop-grid")
    public String productos() {
@@ -49,46 +64,7 @@ public class PortalControlador {
    public String mi() {
        return "my-account.html";
   }
-    @PostMapping("/registrar")
-   public String registrar(ModelMap modelo, String nombre, String email, String tel, String clave, Date alta){
-       try {
-    	   System.out.println(nombre+ email+ tel+ clave);
-       usuarioServicio.registrar(nombre, clave, email, tel, alta);
-       } catch (ErrorServicio ex) {
-           modelo.put("nombre", nombre);
+   
 
-     modelo.put("mail", email);
-     modelo.put("tel", tel);
-  modelo.put("clave", clave);
-       return "index.html";
-       }
-   /* @PostMapping("/registrar")
-//   public String registrar(ModelMap modelo, String nombre, String apellido, String mail, String clave, String grupo_sanguineo,
-   		String zona, String edad, String peso,
-   		String medicacion, String alergias, String antecedentes){
-       try {
-    	   System.out.println(nombre+  apellido+  mail+  clave+  grupo_sanguineo+
-           		 zona+  Integer.parseInt(edad)+  Integer.parseInt(peso)+  
-        		 medicacion+  alergias+  antecedentes);
-           usuarioServicio.registrar( nombre,  apellido,  mail,  clave,  grupo_sanguineo,
-           		 zona,  Integer.parseInt(edad),  Integer.parseInt(peso), 
-        		 medicacion,  alergias,  antecedentes);
-       } catch (ErrorServicio ex) {
-          
-//           modelo.put("error", ex.getMessage());
-//           modelo.put("nombre", nombre);
-//           modelo.put("apellido", apellido);
-//           modelo.put("mail", mail);
-//           modelo.put("clave", clave);
-//           modelo.put("zona", idZona);
-//           modelo.put("grupo_sanguineo", grupo_sanguineo);
-//           modelo.put("requisitos", id_requisitos);
-          return "registro.html";
-           
-       }
-       return "index.html";
-       }
-}*/
-        return null;
 }
-}
+
